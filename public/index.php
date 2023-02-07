e917e524e1534cc6a92847399c62e1248bbca993
Vladimir Ugarov <graciadan@rambler.ru>
2023-02-07 23:32:26 +0300

    step 2

--- public/index.php
+++ public/index.php
@@ -3,14 +3,24 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
+session_start();
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($req, $resp) {
+    return $this -> get('renderer') -> render($resp, 'main.phtml');
+});
+
 $app->run();
