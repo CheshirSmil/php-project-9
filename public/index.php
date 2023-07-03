5d23465fc63570e49f22950093984e3ee7a6ad90
Vladimir Ugarov <graciadan@rambler.ru>
2023-07-03 23:50:07 +0300

    refactoring code

--- public/index.php
+++ public/index.php
@@ -152,7 +152,6 @@ $app->post('/urls', function ($request, $response) use ($router) {
         $params = [
             'url' => $formData['name'],
             'errors' => $errors,
-            'invalidForm' => 'is-invalid'
         ];
         return $this->get('view')->render($response->withStatus(422), 'main.twig.html', $params);
     }
