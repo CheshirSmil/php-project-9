02871f663e80cc90b11a2376efdab5c0c7bb692f
Vladimir Ugarov <graciadan@rambler.ru>
2023-07-16 16:02:19 +0300

    fix lint

--- public/index.php
+++ public/index.php
@@ -73,7 +73,7 @@ $customErrorHandler = function (
 ) use ($app) {
     $actualCode = $exception->getCode();
     $response = $app->getResponseFactory()->createResponse();
-    if ($actualCode === 404) {q
+    if ($actualCode === 404) {
         return $this->get('view')->render($response, '404.twig.html');
     }
 };
